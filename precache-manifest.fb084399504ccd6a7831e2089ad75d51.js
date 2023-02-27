29b18ea614256613b9cff8320e90c66ab9e33a92
Ajay-oraon <52890890+Ajay-oraon@users.noreply.github.com>
2023-02-27 14:01:44 +0530

    Updates

---
+++ precache-manifest.fb084399504ccd6a7831e2089ad75d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c04d552246c09bf9f1f",
+    "url": "/MyTxtUtils/static/css/main.51b0d576.chunk.css"
+  },
+  {
+    "revision": "5c04d552246c09bf9f1f",
+    "url": "/MyTxtUtils/static/js/main.5c04d552.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/MyTxtUtils/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "18041bff3c3dce2d1ab1",
+    "url": "/MyTxtUtils/static/js/2.18041bff.chunk.js"
+  },
+  {
+    "revision": "4109c6da960111500662",
+    "url": "/MyTxtUtils/static/js/runtime~main.4109c6da.js"
+  },
+  {
+    "revision": "76efd26a2a86a1e5cfe9ffc5088ce28b",
+    "url": "/MyTxtUtils/index.html"
+  }
+];
